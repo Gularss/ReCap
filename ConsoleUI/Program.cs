b43dee5a0561cd0759d411664ea4fe076ca53e9a
Gülden Sönmez <gulden.arslanoglu@boun.edu.tr>
2021-02-11 23:34:03 +0300

    Version0.2

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,68 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.ModelYear + " " + car.Description );
+
+            }
+
+            //Car carById = carManager.GetById(1);
+            //Console.WriteLine(carById.DailyPrice);
+
+            //Car carToAdd = new Car()
+            //{
+            //    Id = 5,
+            //    BrandId = 2,
+            //    ColorId = 3,
+            //    DailyPrice = 100000,
+            //    ModelYear = 2021,
+            //    Description = "Son model"
+            //};
+            //carManager.Add(carToAdd);
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Id + " " + car.ModelYear + " " + car.Description);
+
+            //}
+
+            //Car carToUpdate = new Car()
+            //{
+            //    Id = 1,
+            //    BrandId = 3,
+            //    ColorId = 1,
+            //    DailyPrice = 40000,
+            //    Description = "Güncel Versiyon",
+            //    ModelYear = 2005
+            //};
+            //carManager.Update(carToUpdate);
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Id + " " + car.ModelYear + " " + car.Description);
+
+            //}
+
+            
+            //Console.WriteLine("Hello World!");
+
+            ////Delete Car 2
+            //Console.WriteLine("Silmek istediğiniz aracın Id sini giriniz:");
+            //int v = Convert.ToInt32(Console.ReadLine());
+            //int delNo = v;
+            //Car carToDelete = carManager.GetById(delNo);
+            //carManager.Delete(carToDelete);
+            //Console.WriteLine("Araç silindi: " + carToDelete.Id + ":" + carToDelete.Description + "\n");
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=220000,Description="Full Paket"},
+                new Car{Id=2,BrandId=2,ColorId=1,ModelYear=2002,DailyPrice=150000,Description="Eco Paket"},
+                new Car{Id=3,BrandId=3,ColorId=2,ModelYear=2010,DailyPrice=450000,Description="Hatcback"},
+                new Car{Id=4,BrandId=2,ColorId=3,ModelYear=2015,DailyPrice=320000,Description="Klimalı"},
+            };
+        }
+        
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetById(int carId)
+        {
+            Car carGetById = _cars.SingleOrDefault(c => c.Id == carId);
+            return carGetById;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
